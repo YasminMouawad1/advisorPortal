389ff46807b0f0c79969a6715030d846f9b749d8
YasminMouawad1 <111860564+YasminMouawad1@users.noreply.github.com>
2023-08-17 12:41:35 +0300

    init repo

---
+++ src/app/views/layout/customers/customers.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomersRoutingModule } from './customers-routing.module';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
+import { CustomersItemComponent } from './customers-item/customers-item.component';
+ 
+import { NgxPaginationModule } from 'ngx-pagination'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core'; 
+
+
+@NgModule({
+  declarations: [
+    CustomersComponent,
+    CustomersListComponent,
+    CustomersItemComponent
+  ],
+  imports: [
+    CommonModule,
+    CustomersRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule,
+    NgxPaginationModule
+  ]
+})
+export class CustomersModule { }
